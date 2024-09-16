4ceef799f55358838aaa84e64f3c2e7c70c3c01c
utsav1o1 <utsavkarki244@gmail.com>
2024-09-16 17:38:32 +0545

    authentication and dashboard

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,11 +1,62 @@
-exports.dashboard = async (req, res)=> {
+const Note = require('../models/Note');
+const mongoose = require('mongoose');
+
+exports.dashboard = async (req, res) => {
+    let perPage = 12;
+    let page = req.query.page || 1;
+
     const locals = {
-        title:'Dashboard',
-        description:'This is a simple note taking app built with NodeJs and ExpressJs',
-        userName: 'Utsav Karki',
+        title: 'Dashboard',
+        description: 'This is a simple note taking app built with NodeJs and ExpressJs',
+
     }
+
+    const notes = await Note.aggregate([
+        { $sort: { updatedAt: -1 } },
+        { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+        
+        {
+            $project: {
+                title: { $substr: ["$title", 0, 30] },
+                body: { $substr: ["$body", 0, 100] },
+            },
+        }
+    ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+
+   
+        const count = await Note.countDocuments({ user: req.user.id });
+
     res.render('dashboard/index', {
-        locals,
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: "../views/layouts/dashboard",
+      current: page,
+      pages: Math.ceil(count / perPage)
+    });
+
+   
+}
+
+exports.add = (req, res) => {
+
+    res.render('dashboard/add', {
         layout: '../views/layouts/dashboard'
     });
+}
+
+exports.addNote = async (req, res) => {
+    console.log(req.body);
+    const { title, content } = req.body;
+    const note = new Note({
+        title,
+        content,
+        user: req.user.id
+    });
+    await note.save();
+    res.redirect('/dashboard');
 }
